1eea5538bf45b018ca4b9bb1029828c39e7539a6
Dinkar Kumar <dinkarkumar166@gmail.com>
2026-08-30 21:24:59 +0530

    SwastyaSetu

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,12 +14,12 @@ const app = express();
 
 
 // Middleware
-// app.use(
-//     cors({
-//         origin: process.env.CLIENT_URL,
-//         credentials: true
-//     })
-// );
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true
+    })
+);
 
 app.use(express.json());
 
